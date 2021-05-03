6e0078cc2eade4db32c25f3033952350befcba3b
Thinh Phan <thinh.phan.18@cnu.edu>
2021-05-03 17:17:15 -0400

    add some test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, queryByText } from '@testing-library/react';
 import App from './App';
 
 test('test render componenets', () => {
@@ -16,3 +16,49 @@ test('test render componenets', () => {
   expect(contactElement).toBeInTheDocument();
 
 });
+
+it('test contact componenets' , () => {
+  const { queryByPlaceholderText } = render(<App/>);
+
+  expect(queryByPlaceholderText('First Name')).toBeTruthy();
+});
+
+describe('test input value' , () => {
+  it ('update onChange', () => {
+    const { queryByPlaceholderText } = render(<App/>);
+
+    const firstNameInput = queryByPlaceholderText('First Name');
+    const lasttNameInput = queryByPlaceholderText('Last Name');
+    const emailInput = queryByPlaceholderText('Email');
+
+    fireEvent.change(firstNameInput, {target: {value: 'Thinh'}});
+    fireEvent.change(lasttNameInput, {target: {value: 'Phan'}});
+    fireEvent.change(emailInput, {target: {value: 'phan.tthinh1@gmail.com'}});
+
+    expect(firstNameInput.value).toBe('Thinh');
+    expect(lasttNameInput.value).toBe('Phan');
+    expect(emailInput.value).toBe('phan.tthinh1@gmail.com');
+
+  })
+})
+
+describe('submit contact form', () => {
+  it('test submit', () => {
+    const { queryByPlaceholderText, queryByText } = render(<App/>);
+
+    const firstNameInput = queryByPlaceholderText('First Name');
+    const lasttNameInput = queryByPlaceholderText('Last Name');
+    const emailInput = queryByPlaceholderText('Email');
+    const messageInput = queryByPlaceholderText('Message');
+
+    fireEvent.change(firstNameInput, {target: {value: 'Thinh'}});
+    fireEvent.change(lasttNameInput, {target: {value: 'Phan'}});
+    fireEvent.change(emailInput, {target: {value: 'phan.tthinh1@gmail.com'}});
+    fireEvent.change(messageInput, {target: {value: 'portfolio message'}});
+
+    fireEvent.click(queryByText("Submit"))
+
+    // const expected = {firstname: 'Thinh', lastname: 'Phan', email: 'phan.tthinh1@gmail.com', message: 'portfolio message'};
+
+  })
+})

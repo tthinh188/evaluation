478d9cf308ef0323aec5326f1220a7442bccf6a1
Thinh Phan <thinh.phan.18@cnu.edu>
2021-05-04 13:58:23 -0400

    some change

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import './boostrap/css/bootstrap.min.css';
 import About from './componenets/About/About';
@@ -7,8 +8,59 @@ import Main from './componenets/Main/Main';
 import Navbar from './componenets/Navbar/Navbar';
 import Projects from './componenets/Projects/Projects';
 import Summary from './componenets/Summary/Summary';
+import $ from 'jquery';
 
 function App() {
+  useEffect(() => {
+    const navigation = () =>{
+        $('.primary-nav').css('height', $('.logo').height());
+        $('.primary-nav li').css(
+          'margin-top',
+          ($('.primary-nav').height() - $('.primary-nav li').height()) / 2 + 'px'
+        );
+    }
+
+    setTimeout(() => {
+      console.log('set');
+      navigation();
+    }, 500);
+  }, [])
+
+  useEffect(() => {
+    function projects() {
+      $('.luvtalk-project').css('height', $('.luvtalk-project').width());
+      $('.luvtalk-project h3').css(
+        'padding-top',
+        ($('.luvtalk-project').height() -
+          $('.luvtalk-project h3').height() -
+          $('.luvtalk-project .row').outerHeight()) /
+          2
+      );
+      $('.personal-website-project').css(
+        'height',
+        $('.personal-website-project').width()
+      );
+      $('.personal-website-project h3').css(
+        'padding-top',
+        ($('.personal-website-project').height() -
+          $('.personal-website-project h3').height() -
+          $('.personal-website-project .row').outerHeight()) /
+          2
+      );
+      $('.strike-zone-project').css('height', $('.strike-zone-project').width());
+      $('.strike-zone-project h3').css(
+        'padding-top',
+        ($('.strike-zone-project').height() -
+          $('.strike-zone-project h3').height() -
+          $('.strike-zone-project .row').outerHeight()) /
+          2
+      );
+    }
+    
+    projects();
+    
+  }, [])
+
   return (
     <div className="app">
       <Navbar />
